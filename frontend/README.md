40bb961d2b038fd3de20aeb168a3b9ea7d844c51
Adam Domurad <adamdomurad@gmail.com>
2021-05-25 18:41:42 -0400

    Initial front-end commit
    This uses tailwindcss and craco behind the scenes.
    The page uses a tailwindcss dashboard example

---
+++ frontend/README.md
@@ -0,0 +1,3 @@
+1) Run `npm install`
+2) Run `npm start`
+3) Look at page at localhost:3000
---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./index.css";
+import { Demo } from "./Demo";
+
+function App() {
+  return <Demo username="juggyfgc" />;
+}
+
+export default App;
