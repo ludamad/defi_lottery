ed0a95c161d8d1cc3c2d50e50ea3afb3504d5031
Trystan Hendricks <dolkhound@gmx.com>
2021-05-25 18:37:35 -0400

    2nd deploy script not working should be fixed

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -3,15 +3,5 @@ pragma solidity >=0.4.22 <0.9.0;
 import '../node_modules/@chainlink/contracts/src/v0.6/ChainlinkClient.sol';
 
 contract HelloWorld is ChainlinkClient {
-  string public message;
-
-  // When deploying contract a message is needed. Will use Gas
-  constructor(string memory initMessage) public {
-    message = initMessage;
-  }
-
-  // Updates the initial message USES GAS
-  function updateMessage(string memory newMessage) public {
-    message = newMessage;
-  }
+  string public greet = 'Hello World!';
 }
